79917b3ba0cf62f1f9a7904f2f571f79341dfd50
samtenali123-cyber <samtenali123@gmail.com>
2026-02-13 20:52:33 +0000

    changed the code part A

---
+++ PartA.py
@@ -0,0 +1,31 @@
+# partA.py
+# this program prints steps 1 to 10 on the computer screen
+# Sam Tenali, b00171391
+# 09/02/2026
+
+def printSteps():
+    for i in range(4,7):
+        print(f"step {i}")
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    print("Step 1")
+    print("Step 2")
+    print("Step 3")
+
+    printSteps()
+    print("Step 7")
+    print("Step 8")
+    print("Step 9")
+
+if__main__=="__main__":
+    main()
