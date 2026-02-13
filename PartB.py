eadb44e3354664bf92129537058ff21ecb7c715f
samtenali123-cyber <samtenali123@gmail.com>
2026-02-13 21:01:29 +0000

    completed part B

---
+++ PartB.py
@@ -0,0 +1,35 @@
+# partB.py
+# this program prints steps 1 to 10 on the computer screen, this now incorporates
+# a function printSteps(a,b) a is the starting value and b is the end value for a loop
+# this loop prints the terms step n based on the the arguments
+# Sam Tenali, b00171391
+# 09/02/2026
+
+def printSteps(a, b):
+    for i in range(a,b+1):
+        print(f"step {i}")
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    print("Step 1")
+    print("Step 2")
+    print("Step 3")
+
+    printSteps(4,6)
+
+    print("Step 7")
+    print("Step 8")
+    print("Step 9")
+
+if__main__=="__main__":
+    main()
+
